2416d582a0e28d0b0900b4137ac46555e31cf2ef
Van <MisterDeterminator@yandex.ru>
2025-06-26 22:21:20 +0000

    build

--- src/pages/license/LicensePage.tsx
+++ src/pages/license/LicensePage.tsx
@@ -2,7 +2,44 @@ import IconButton from "@/shared/components/IconButton";
 import Carousel from "@/widgets/carousel/Carousel";
 import Header from "@/widgets/header/Header";
 import Image from "next/image";
-import { carouselData } from "./constants";
+
+const carouselData = [
+  {
+    imageUrl: "/images/licenses/Сертификат_ИСО_9001-2015_2024-1_1.png",
+    alt: "Сертификат ИСО 9001-2015_2024-1 1",
+    title: (
+      <div>
+        <span className="font-(family-name:--font-sf-ui-display) font-bold text-[25px] leading-[0.9]">
+          Сертификат ИСО Серконс
+        </span>
+        <div className="w-[25px] h-[7px] bg-(--color-blue) mt-[35px]" />
+        <span className="text-(--color-text-dark) text-(length:--main-text-size) max-w-[250px] mt-[35px] inline-block">
+          2024 - 2027
+        </span>
+      </div>
+    ),
+  },
+  {
+    imageUrl: "/images/licenses/Сертификат_ИСО_Серконс_2024-2027-1_2.png",
+    alt: "Сертификат_ИСО_Серконс_2024-2027-1_2",
+    title: (
+      <div>
+        <span className="font-(family-name:--font-sf-ui-display) font-bold text-[25px] leading-[0.9]">
+          Сертификат ИСО Серконс
+        </span>
+        <div className="w-[25px] h-[7px] bg-(--color-blue) mt-[35px]" />
+        <span className="text-(--color-text-dark) text-(length:--main-text-size) max-w-[250px] mt-[35px] inline-block">
+          2024 - 2027
+        </span>
+      </div>
+    ),
+  },
+  {
+    imageUrl: "/images/licenses/Сертификат_ИСО_9001-2015_2024-1_1.png",
+    alt: "Сертификат ИСО 9001-2015_2024-1 1",
+    title: <div>сертификат</div>,
+  },
+];
 
 const LicensePage = () => (
   <div>
