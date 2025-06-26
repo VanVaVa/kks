5c2c4aa6c2703c9f4cac8b9dadd9f648de17668b
Van <MisterDeterminator@yandex.ru>
2025-06-26 22:37:14 +0000

    pizda

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  images: {
+    unoptimized: true, // Отключаем оптимизацию изображений
+  },
 };
 
 export default nextConfig;
